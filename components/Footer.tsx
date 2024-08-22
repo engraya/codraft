85768e06f92f6b800e2e54493c6d4102c5941083
Engraya <ahmadyakubuaya@gmail.com>
2024-08-22 16:21:22 +0100

    Trigger Deployment Commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="text-white py-4">
+    <footer className="text-white py-3">
     <div className="container mx-auto text-center">
       <p>© 2024 CoDraft. All rights reserved.</p>
     </div>
